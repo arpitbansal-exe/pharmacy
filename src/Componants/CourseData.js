89f0e8b634270a0456db274315630bfc543b77f9
Arpit Bansal <arpitbansal2304@gmail.com>
2023-11-18 22:37:31 +0530

    small

--- src/Componants/CourseData.js
+++ src/Componants/CourseData.js
@@ -1,9 +1,8 @@
 export const course_data={
     bpharma:{
-        Course_name:"Bachelor of Pharmacy",
-        Course_duration:"4 Years",
-        Course_type:"Undergraduate",
-        Course_level:"Degree",
+        Course_name:"Diploma in Pharmacy",
+        Course_duration:" 2 Years",
+        Course_type:"Diploma",
         Course_eligibility:"10+2 with PCB/PCM",
         Course_description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam eu nulla tincidunt tincidunt. Etiam nec urna nec "
     }
--- src/Pages/CourseDetail.js
+++ src/Pages/CourseDetail.js
@@ -9,7 +9,6 @@ export default function CourseDetail(props) {
           <h1>Course Name:  {props.Course_name}</h1>
           <h1>Course Duration: {props.Course_duration}</h1>
           <h1>Course Type: {props.Course_type}</h1>
-          <h1>Course Level: {props.Course_level}</h1>
           <h1>Course Eligibility: {props.Course_eligibility}</h1>
           <h1>Course Description: {props.Course_description}</h1>
 
