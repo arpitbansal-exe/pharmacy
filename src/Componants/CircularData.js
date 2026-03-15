f0b24b229b096f112ccaf66dd7fb444252366c91
Arpit Bansal <arpitbansal2304@gmail.com>
2026-03-15 23:38:28 +0530

    Update Circular info

--- src/Componants/CircularData.js
+++ src/Componants/CircularData.js
@@ -1,14 +1,14 @@
 export const circularsData=[
     {
       "id": 1,
-      "heading": "RUHS Affilication letter",
+      "heading": "RUHS Affilication Letter",
       "description": "Affiliation letter for D.Pharm course",
       "link": "https://drive.google.com/file/d/157lnKZEMybv_oHqrdUw3tPbUcjTXBARx/view?usp=sharing"
     },
     {
       "id": 2,
-      "heading": "PCI Affiliation letter",
-      "description": "D.Pharm Grant approval for latest academic session",
+      "heading": "PCI Decision Letter",
+      "description": "D.Pharm Grant last approval letter",
       "link": "https://drive.google.com/file/d/1qnVebqY5c6RRN_a1md9eLNLiIQpQphjw/view?usp=sharing"
     },
     // Add more circulars as needed
